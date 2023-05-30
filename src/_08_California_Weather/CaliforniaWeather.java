bc7c7397e610bfd64800192f67b9e1b8a2e8c4a1
Jackson-Bush <bushjackson56@gmail.com>
2023-05-29 18:29:10 -0700

    weather

--- src/_08_California_Weather/CaliforniaWeather.java
+++ src/_08_California_Weather/CaliforniaWeather.java
@@ -1,5 +1,8 @@
 package _08_California_Weather;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JButton;
@@ -32,36 +35,77 @@ import javax.swing.JPanel;
  * temperature, you can get a free API key at: https://openweathermap.org/api
  */
 
-public class CaliforniaWeather {
+public class CaliforniaWeather implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button1 = new JButton("Find Weather of City");
 	JButton button2 = new JButton("Find city with weather condition");
 	JButton button3 = new JButton("Find city with Min: __ and Max:__ temp in F");
+	
     void start() {
-    	
-        HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
-        String inputCity = JOptionPane.showInputDialog("What city's weather would you like to search for?");
-        
-        // All city keys have the first letter capitalized of each word
-        String cityName = Utilities.capitalizeWords( inputCity);
-        WeatherData datum = weatherData.get(cityName);
-        
-        if( datum == null ) {
-            System.out.println("Unable to find weather data for: " + cityName);
-        } else {
-            System.out.println(cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
-        }
+    	panel.add(button1);
+    	panel.add(button2);
+    	panel.add(button3);
+    	frame.add(panel);
+    	frame.setSize(300, 300);
+    	frame.setVisible(true);
+    	frame.pack();
+    	button1.addActionListener(this);
+    	button2.addActionListener(this);
+    	button3.addActionListener(this);
+       
         
         
-        String inputWeather = JOptionPane.showInputDialog("What weather condition would you like to search for? ");
+        
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
+		if (e.getSource()== button1) {
+			String inputCity = JOptionPane.showInputDialog("What city's weather would you like to search for?");
+	        
+	        // All city keys have the first letter capitalized of each word
+	        String cityName = Utilities.capitalizeWords( inputCity);
+	        WeatherData datum = weatherData.get(cityName);
+	        
+	        if( datum == null ) {
+	        	JOptionPane.showMessageDialog(null, "Unable to find weather data for: " + cityName);
+	            System.out.println();
+	        } else {
+	        	JOptionPane.showMessageDialog(null,cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
+	           
+	        }
+	        
+		}
+	if(e.getSource()==button2) {
+		ArrayList <String> cities = new ArrayList<String>(); 
+		String weatherCondition = JOptionPane.showInputDialog("What weather condition would you like to search for? ");
         for(String i : weatherData.keySet()) {
-        	WeatherData weatherF = weatherData.get(i);
-        	System.out.println(weatherF);
-        	if (weatherF.equals(inputWeather)) {
+        	if(weatherData.get(i).weatherSummary.equals(weatherCondition)) {
         		System.out.println(i);
+        		cities.add(i);
         	}
         	
-        }
-    }
 }
+        JOptionPane.showMessageDialog(null, "These cities have the weather condition you are looking for: " + cities);
+        }
+	if(e.getSource() == button3) {
+		ArrayList <String> cities = new ArrayList<String>(); 
+		String maxTemp = JOptionPane.showInputDialog("What max temperature: ");
+		String minTemp = JOptionPane.showInputDialog("What minimum temperature: ");
+		Double max = Double.parseDouble(maxTemp);
+		Double min = Double.parseDouble(minTemp);
+		
+		for (String i : weatherData.keySet()) {
+			if(weatherData.get(i).temperatureF<=max&& weatherData.get(i).temperatureF >= min) {
+				cities.add(i);
+				
+			}
+		}
+		JOptionPane.showMessageDialog(null, "These cities have the temperature you are looking for: "+ cities);
+	}
+	}
+		
+	}
+
